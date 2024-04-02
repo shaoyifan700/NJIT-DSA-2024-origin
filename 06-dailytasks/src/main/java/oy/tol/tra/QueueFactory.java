82a985f4796aa15be537f545e1455ab5fa8fa89a
shaoyifan700 <731104277@qq.com>
2024-04-02 09:19:41 +0800

    0402first

---
+++ 06-dailytasks/src/main/java/oy/tol/tra/QueueFactory.java
@@ -0,0 +1,48 @@
+package oy.tol.tra;
+
+/**
+ * This class instantiates a queue implementing the {@code QueueInterface}.
+ * 
+ * @author Antti Juustila
+ */
+public class QueueFactory {
+
+   private QueueFactory() {
+   }
+
+   /**
+    * Creates an instance of QueueInterface for Integer type.
+    * @param capacity Number of elements the queue can hold.
+    * @return The queue object.
+    */
+   public static QueueInterface<Integer> createIntegerQueue(int capacity) {
+      // TODO: Implement this when you have finished your QueueImplementation.
+      // - Instantiates your queue implementation using Integer as template parameter, 
+      //   with the given capacity,
+      // - and return the object to the caller.
+      QueueInterface<Integer> queue = new QueueImplementation<>(capacity);
+      return queue;  
+   }
+
+   /**
+    * Creates an instance of QueueInterface for Integer type.
+    * Use the default constructor of the queue.
+    * @return The queue object.
+    */
+   public static QueueInterface<Integer> createIntegerQueue() {
+   //    // TODO: Implement this when you have finished your QueueImplementation.
+   //    // - Instantiates your queue implementation using Integer as template parameter, 
+   //    //   with default capacity,
+   //    // - and return the object to the caller.
+      QueueInterface<Integer> queue = new QueueImplementation<>();
+      return queue;    
+    }
+    
+    public static QueueInterface<String> createStringQueue(int capacity) {
+      return new QueueImplementation<>(capacity);
+  }
+  
+  public static QueueInterface<String> createStringQueue() {
+      return new QueueImplementation<>();
+  }
+}
